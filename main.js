f7a37835264ce0e2143c92531a2b3ebde72e0821
Акрам <115643985+akramblch@users.noreply.github.com>
2022-10-18 17:35:36 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+// задание 1
+// const input = document.querySelector('#input2')
+// const btn = document.querySelector('#btn')
+
+// let age = 14
+
+// btn.addEventListener('click', () => {
+//     let num = new Promise((resolve, reject) => {
+//         if(input.value == age) {
+//             resolve('Вы угадали!')
+//         }else {
+//             reject('Попробуйте еще раз')
+//         }
+//     }).then((resolve) => console.log(resolve))
+//         .catch((reject) => console.log(reject))
+    
+// })
+
+
+// задание 2
+
+// let jah = 10
+
+// btn.addEventListener('click', () => {
+//         let num = new Promise((resolve, reject) => {
+//             if(input.value == jah) {
+//                 alert('Правильно!')
+//             }else {
+//                 alert('Не верно')
+//             }
+//         })
+// })
